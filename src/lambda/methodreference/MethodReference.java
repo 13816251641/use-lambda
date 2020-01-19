3aa8da759ce289c878660032ecfd820c774af1fc
admin <982584989@qq.com>
2020-01-19 16:55:07 +0800

    代码提交

--- src/lambda/methodreference/MethodReference.java
+++ src/lambda/methodreference/MethodReference.java
@@ -1,8 +1,10 @@
 package lambda.methodreference;
 
+import lombok.Data;
 import org.junit.Test;
 import wangwenjun.sample.Apple;
 import wangwenjun.sample.ComplexApple;
+import wangwenjun.sample.Person;
 import wangwenjun.sample.ThreeFunction;
 
 import java.util.Arrays;
@@ -15,7 +17,6 @@ import java.util.function.*;
  * 测试使用方法推导
  */
 public class MethodReference {
-
     private static <T> void useConsumer(Consumer<T> consumer, T t) {
         consumer.accept(t);
     }
@@ -24,27 +25,24 @@ public class MethodReference {
     public void testUseConsumer(){
         useConsumer(s -> System.out.println(s), "Hello Alex");
         useConsumer(System.out::println, "Hello Wangwenjun");
-
     }
-
-
 
 
     @Test
     public void testList(){
+
         List<Apple> list = Arrays.asList(new Apple("abc", 123), new Apple("Green", 110), new Apple("red", 123));
 
-        System.out.println(list);
-
+        /* Comparator<? super Apple> 因为入参都是Apple或者Apple的子类,所以lambda用Apple来接收没毛病 */
         list.sort((a1, a2) -> a1.getColor().compareTo(a2.getColor()));
 
         System.out.println(list);
 
-        list.stream().forEach(a -> System.out.println(a));//遍历
+        list.stream().forEach(a -> System.out.println(a));//普通遍历
 
         System.out.println("==========================");
 
-        list.stream().forEach(System.out::println);
+        list.stream().forEach(System.out::println);//方法推导
 
     }
 
@@ -68,8 +66,8 @@ public class MethodReference {
         System.out.println(c2);
 
         String s12 = new String("hello123");
-        IntSupplier runnable = s12::length;
-        System.out.println(runnable.getAsInt());
+        IntSupplier intSupplier = s12::length;
+        System.out.println(intSupplier.getAsInt());
 
         Supplier<String> supplier = String::new;
         String s = supplier.get();
@@ -82,10 +80,12 @@ public class MethodReference {
      * @param
      */
     public void testApple(){
+        /* new Apple对象需要String和Long构成构造方法,返回一个Apple */
         BiFunction<String, Long, Apple> appleFuntion = Apple::new;
         Apple apple = appleFuntion.apply("red", 100L);
         System.out.println(apple);
 
+        /* new ComplexApple对象需要String,Long,Long,并返回ComplexApple*/
         ThreeFunction<String, Long, String, ComplexApple> threeFunction = ComplexApple::new;
         ComplexApple complexApple = threeFunction.apply("Green", 123L, "FuShi");
         System.out.println(complexApple);
