270372cc245c30e13f2fd97aee93174d682c327c
admin <982584989@qq.com>
2020-02-09 15:59:31 +0800

    代码提交

---
+++ src/lambda/stream/MatchStream.java
@@ -0,0 +1,40 @@
+package lambda.stream;
+
+import org.junit.Test;
+
+import java.util.stream.Stream;
+
+/**
+ * @Auther ljn
+ * @Date 2020/2/9
+ */
+public class MatchStream {
+    private Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5);
+
+    /**
+     * 必须都大于3
+     */
+    @Test
+    public void test01(){
+        boolean b = stream.allMatch(n -> n > 3);
+        System.out.println(b);
+    }
+
+    /**
+     * 任意一个大于3即可
+     */
+    @Test
+    public void test02(){
+        boolean b = stream.anyMatch(n -> n > 3);
+        System.out.println(b);
+    }
+
+    /**
+     * 没有一个小于0
+     */
+    @Test
+    public void test03(){
+        boolean b = stream.noneMatch(n->n<0);
+        System.out.println(b);
+    }
+}
